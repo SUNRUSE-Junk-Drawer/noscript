a5bcd0a2546b941b5fc3cb0cb4bca2a1231517a1
jameswilddev <jameswilddev@outlook.com>
2018-07-06 08:13:58 +0100

    Added placeholder game code.

---
+++ src/game/game.c
@@ -0,0 +1,9 @@
+void se_game_initialize() {
+  se_platform_critical_stop("TODO: se_game_initialize.");
+}
+
+void se_game_process(float deltaSeconds) {
+  se_platform_critical_stop("TODO: se_game_process.");
+}
+
+void se_game_render() { se_platform_critical_stop("TODO: se_game_render."); }
