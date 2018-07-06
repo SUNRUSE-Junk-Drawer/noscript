9b6348ec2c576d361c0240ed94fb056b35f54ab0
jameswilddev <jameswilddev@outlook.com>
2018-07-06 08:13:27 +0100

    Began defining game code types.

---
+++ src/game/game.h
@@ -0,0 +1,8 @@
+#define SE_GAME_NAME "ExampleSpartanEngineGame"
+#define SE_GAME_TITLE "Example Spartan Engine Game"
+
+void se_game_initialize();
+
+void se_game_process(float deltaSeconds);
+
+void se_game_render();
