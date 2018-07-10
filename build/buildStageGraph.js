c15dc644c33ab9160831bbf16f26ff59306f73fe
jameswilddev <jameswilddev@outlook.com>
2018-07-10 07:45:06 +0100

    The build stage graph is now built from the global list rather than recursing dependencies (this will include orphaned build stages).

--- build/buildStageGraph.js
+++ build/buildStageGraph.js
@@ -2,6 +2,7 @@ import * as fs from "fs"
 import * as path from "path"
 import mkdirp from "mkdirp"
 import BuildStage from "./buildStage"
+import * as buildStage from "./buildStage"
 
 import deletePreviousBuilds from "./deletePreviousBuilds"
 
@@ -11,22 +12,19 @@ class BuildStageGraphStage extends BuildStage {
   }
 
   performStart() {
-    const buildStages = []
-
-    const recurse = buildStage => {
-      if (buildStages.indexOf(buildStage) != -1) {
-        return
-      }
-
-      buildStages.push(buildStage)
-
-      buildStage.dependencies.forEach(recurse)
-      buildStage.dependents.forEach(recurse)
-    }
-
-    recurse(this)
-
     const distPath = path.join(`dist`, `graphs`)
+
+    const nodes = buildStage.all
+      .map(buildStage => `[${buildStage.name}]`)
+      .join(`\n`)
+
+    const links = buildStage.all
+      .map(dependency => dependency.dependents
+        .map(dependent => `[${dependency.name}] -> [${dependent.name}]`)
+        .join(`\n`))
+      .join(`\n`)
+
+    const graph = `${nodes}\n${links}`
 
     this.log(`Creating dist path "${distPath}"...`)
     mkdirp(distPath, error => {
@@ -34,14 +32,10 @@ class BuildStageGraphStage extends BuildStage {
 
       const graphPath = path.join(distPath, `buildStages.nomnoml`)
       this.log(`Writing to "${graphPath}"...`)
-      fs.writeFile(graphPath, buildStages
-        .map(buildStage => buildStage.dependents
-          .map(dependent => `[${buildStage.name}] -> [${dependent.name}]`)
-          .join(`\n`)
-        ).join(`\n`), error => {
-          this.handle(error)
-          this.done()
-        })
+      fs.writeFile(graphPath, graph, error => {
+        this.handle(error)
+        this.done()
+      })
     })
   }
 }
