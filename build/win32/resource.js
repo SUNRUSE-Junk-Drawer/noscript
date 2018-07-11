2d85d7a6d226a262ff7a693b741542a1db358933
jameswilddev <jameswilddev@outlook.com>
2018-07-11 07:24:25 +0100

    A resource file is now generated as part of a win32 build.

---
+++ build/win32/resource.js
@@ -0,0 +1,24 @@
+import * as commandExists from "command-exists"
+import * as childProcess from "child_process"
+import * as path from "path"
+import BuildStage from "./buildStage"
+
+import icon from "./icon"
+import manifest from "./manifest"
+
+class ResourceBuildStage extends BuildStage {
+  constructor() {
+    super(`resource`, [icon, manifest], !commandExists.sync(`windres`))
+  }
+
+  performStart() {
+    childProcess.execFile(`windres`, [path.join(`src`, `platforms`, `win32.rc`), `-O`, `coff`, `-o`, path.join(`temp`, `win32`, `win32.res`)], (error, stdout, stderr) => {
+      this.handle(error)
+      this.handle(stdout)
+      this.handle(stderr)
+      this.done()
+    })
+  }
+}
+
+export default new ResourceBuildStage()
