fcdecf8719917955fe5fb68b201dfc3ea8415f1b
jameswilddev <jameswilddev@outlook.com>
2018-07-27 18:35:39 +0100

    Added a build stage for reading files as text (#31).

---
+++ build/readTextBuildStage.js
@@ -0,0 +1,21 @@
+import * as path from "path"
+import * as fs from "fs"
+import BuildStage from "./buildStage"
+
+export default class ReadTextBuildStage extends BuildStage {
+  constructor(name, pathSegmentFactory, dependencies) {
+    super(name, dependencies, false)
+    this.pathSegmentFactory = pathSegmentFactory
+  }
+
+  performStart() {
+    fs.readFile(
+      path.join.apply(path, this.pathSegmentFactory()),
+      { encoding: `utf8` },
+      (error, text) => this.handle(error, () => {
+        this.text = text
+        this.done()
+      })
+    )
+  }
+}
