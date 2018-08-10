ae16b35654bc588dcd469f1f32268bb8f063fea7
jameswilddev <jameswilddev@outlook.com>
2018-08-10 19:39:02 +0100

    Merged HTML generation into the wasm build stage.

--- build/wasm/addWasmBuildStages.js
+++ build/wasm/addWasmBuildStages.js
@@ -1,6 +1,7 @@
+import * as htmlMinifier from "html-minifier"
 import CreateDirectoryBuildStage from "./../createDirectoryBuildStage"
-import HtmlBuildStage from "./htmlBuildStage"
 import BootloaderBuildStage from "./bootloaderBuildStage"
+import WriteFileBuildStage from "../writeFileBuildStage"
 
 export default (parent, metadata, createDistDirectory) => {
   const createWasmDistDirectory = new CreateDirectoryBuildStage(
@@ -11,5 +12,37 @@ export default (parent, metadata, createDistDirectory) => {
   )
 
   const bootloader = new BootloaderBuildStage(parent, createWasmDistDirectory)
-  new HtmlBuildStage(parent, metadata, createWasmDistDirectory, bootloader)
+
+  new WriteFileBuildStage(
+    parent,
+    `wasm/html`,
+    () => [`games`, parent.name, `dist`, `wasm`, `index.html`],
+    () => htmlMinifier.minify(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="UTF-8">
+            <title>${metadata.json.applicationName}</title>
+            <meta name="viewport" content="initial-scale=1, minimum-scale=1, maximum-scale=1, width=device-width, height=device-height, user-scalable=no">
+          </head>
+          <body style="background: black">
+            <script>${bootloader.code}</script>
+          </body>
+        </html>
+      `, {
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeEmptyElements: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        removeTagWhitespace: true
+      }
+    ),
+    [createWasmDistDirectory, bootloader]
+  )
 }
