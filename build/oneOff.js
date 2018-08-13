4831c34649c683f97e6c259e91e9cc480a6f5e1c
jameswilddev <jameswilddev@outlook.com>
2018-08-13 21:18:16 +0100

    Removed code to add the "dist" directory in the project root; this has not been used for a while.

--- build/oneOff.js
+++ build/oneOff.js
@@ -1,20 +1,3 @@
-import rimraf from "rimraf"
-import mkdirp from "mkdirp"
 import BuildProcessBuildStage from "./buildProcessBuildStage"
 
-console.log(`Deleting "dist"...`)
-rimraf(`dist`, error => {
-  if (error) {
-    throw error
-  }
-
-  console.log(`Creating "dist"...`)
-  mkdirp(`dist`, error => {
-    if (error) {
-      throw error
-    }
-
-    console.log(`Starting build...`)
-    new BuildProcessBuildStage(true)
-  })
-})
+new BuildProcessBuildStage(true)
