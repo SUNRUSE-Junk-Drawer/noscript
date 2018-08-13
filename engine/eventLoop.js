374fea6db8c4bd03fcb752bd134d94360f6ecfcc
jameswilddev <jameswilddev@outlook.com>
2018-08-13 07:11:55 +0100

    Added a basic event loop.  This needs revisiting, as it currently means that we're triggering audio right as it needs to happen; ideally we'd want to be a little ahead (maybe a quarter second?)

---
+++ engine/eventLoop.js
@@ -0,0 +1,29 @@
+var time = audioContextTime()
+
+var secondsPerBeat = 60 / beatsPerMinute
+var secondsPerTick = secondsPerBeat / ticksPerBeat
+var numberOfTicks = 0
+var numberOfBeats = 0
+var numberOfBars = 0
+
+function eventLoop() {
+  var nextTime = audioContextTime()
+  while (time + secondsPerTick <= nextTime) {
+    tick()
+    if (!(numberOfTicks % ticksPerBeat)) {
+      beat()
+      if (!(numberOfBeats % beatsPerBar)) {
+        bar()
+        numberOfBars++
+      }
+      numberOfBeats++
+    }
+    numberOfTicks++
+
+    time += secondsPerTick
+  }
+
+  requestAnimationFrame(eventLoop)
+}
+
+requestAnimationFrame(eventLoop)
