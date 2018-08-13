d4473879c3e046579e6531f0cba0221135e47c19
jameswilddev <jameswilddev@outlook.com>
2018-08-13 07:12:35 +0100

    Added an example "song".

---
+++ games/example/src/main.js
@@ -0,0 +1,23 @@
+function tick() {
+}
+
+function beat() {
+}
+
+function bar() {
+  kick(0)
+  kick(2.75)
+  if (numberOfBars % 2) {
+    kick(3.5)
+    openHiHat(3.75)
+  }
+  snare(1)
+  snare(2)
+  snare(2.25)
+  snare(3)
+  openHiHat(0)
+  openHiHat(2.75)
+  for (var i = 0.25; i < 4; i += 0.25) {
+    if (i != 2.75 && (i != 3.75 || !(numberOfBars % 2))) closedHiHat(i)
+  }
+}
