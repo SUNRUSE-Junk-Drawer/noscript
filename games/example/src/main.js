ff8e653431ea07b6c74a5290ec4b010ecb2393d8
jameswilddev <jameswilddev@outlook.com>
2018-08-14 21:34:29 +0100

    Added a "pad" to the example.

--- games/example/src/main.js
+++ games/example/src/main.js
@@ -4,6 +4,8 @@ function tick() {
 function beat() {
 }
 
+var minorScale = [-12, -9, -5, 0, 3, 7]
+
 function bar() {
   kick(0)
   kick(2.75)
@@ -20,6 +22,8 @@ function bar() {
   for (var i = 0.25; i < 4; i += 0.25) {
     if (i != 2.75 && (i != 3.75 || !(numberOfBars % 2))) closedHiHat(i)
   }
+
+  pad(0, minorScale, numberOfBars % 4 > 1 ? -3 : 0, beatsPerBar, 4)
 }
 
 function render() {
