4dcf3cd4bbcb9a07cf2b651a1d59219a3681168b
jameswilddev <jameswilddev@outlook.com>
2018-08-14 08:17:46 +0100

    Conversions between "game units" and normalized device coordinates are now calculated every frame.

--- engine/video/context.js
+++ engine/video/context.js
@@ -6,6 +6,10 @@ var gl = c.getContext("webgl", {
 
 var width
 var height
+var xScale
+var yScale
+var actualWidth
+var actualHeight
 
 function prepareFrame() {
   var dpr = devicePixelRatio
@@ -13,5 +17,16 @@ function prepareFrame() {
   height = c.clientHeight * dpr
   c.width = width
   c.height = height
+  var targetAspectRatio = targetAspectRatioHorizontally / targetAspectRatioVertically
+  var actualAspectRatio = width / height
+  if (targetAspectRatio > actualAspectRatio) {
+    xScale = 2 / targetAspectRatioHorizontally
+    yScale = 2 / targetAspectRatioVertically * actualAspectRatio / targetAspectRatio
+  } else {
+    xScale = 2 / targetAspectRatioHorizontally * targetAspectRatio / actualAspectRatio
+    yScale = 2 / targetAspectRatioVertically
+  }
+  actualWidth = 2 / xScale
+  actualHeight = 2 / yScale
   gl.viewport(0, 0, width, height)
 }
