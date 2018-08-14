2cad107f91fe55a65c3e007cf467ccbfd338581f
jameswilddev <jameswilddev@outlook.com>
2018-08-14 07:34:30 +0100

    Fixed canvas size not being adjusted.

--- engine/video/context.js
+++ engine/video/context.js
@@ -11,5 +11,7 @@ function prepareFrame() {
   var dpr = devicePixelRatio
   width = c.clientWidth * dpr
   height = c.clientHeight * dpr
+  c.width = width
+  c.height = height
   gl.viewport(0, 0, width, height)
 }
