7e6c50d021978e0b04699c3754d62d364eba2ee9
jameswilddev <jameswilddev@outlook.com>
2018-08-14 21:34:39 +0100

    Added some test sprites to the example.

--- games/example/src/main.js
+++ games/example/src/main.js
@@ -26,5 +26,34 @@ function bar() {
   pad(0, minorScale, numberOfBars % 4 > 1 ? -3 : 0, beatsPerBar, 4)
 }
 
+var color = parameter(3)
+
+var topRightCoord = fract(uv)
+var topRightValue = max(x(topRightCoord), y(topRightCoord))
+
+var bottomLeftCoord = subtract(float(1), topRightCoord)
+var bottomLeftValue = max(x(bottomLeftCoord), y(bottomLeftCoord))
+
+var testProgram = compileGlsl(
+  vector(
+    multiply(
+      pow(max(topRightValue, bottomLeftValue), float(64)),
+      color
+    ),
+    float(1)
+  )
+)
+
 function render() {
+  useProgram(testProgram)
+  uniformVec3(color, 1, 0, 0)
+  fillBackground()
+
+  uniformVec3(color, 0, 1, 0)
+  fitBackground()
+
+  uniformVec3(color, 0, 0.5, 1)
+  var circular = elapsed * 2 * Math.PI / secondsPerBeat
+  positionedSprite(Math.sin(circular) * 5, Math.cos(circular) * 5, 2, 2)
+  positionedSprite(Math.sin(circular / beatsPerBar) * 3, Math.cos(circular / beatsPerBar) * 3, 2, 2)
 }
