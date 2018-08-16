c349b6efb79ec630bd73ecc10242710c4032d2b5
jameswilddev <jameswilddev@outlook.com>
2018-08-16 22:06:29 +0100

    Tweaked vignette.

--- games/offline/src/vignette.js
+++ games/offline/src/vignette.js
@@ -3,9 +3,18 @@ var vignetteProgram = compileGlsl(
     float(0),
     float(0),
     float(0),
-    divide(
-      distance(uv, vector(float(0), float(0))),
-      float(9)
+    subtract(
+      float(1),
+      pow(
+        divide(
+          float(1),
+          divide(
+            distance(uv, vector(float(0), float(0))),
+            float(6)
+          )
+        ),
+        float(6)
+      )
     )
   )
 )
