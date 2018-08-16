6a3a121d004aa23716b0be28f6456160c1d68113
jameswilddev <jameswilddev@outlook.com>
2018-08-16 07:48:43 +0100

    The three layers of stars now have slightly different colours.

--- games/offline/src/main.js
+++ games/offline/src/main.js
@@ -15,19 +15,19 @@ function render() {
   uniformVec2(starfieldScroll, elapsed * 0.3, 0)
   uniformVec2(starfieldScale, 0.8, 0.8)
   uniformFloat(starfieldRadius, 0.1)
-  uniformVec3(starfieldColor, 2, 2, 2)
+  uniformVec3(starfieldColor, 2, 2, 1.75)
   fillBackground()
 
   uniformVec2(starfieldScroll, elapsed * 0.2, 0)
   uniformVec2(starfieldScale, 1.4, 1.4)
   uniformFloat(starfieldRadius, 0.1)
-  uniformVec3(starfieldColor, 2, 2, 2)
+  uniformVec3(starfieldColor, 1.5, 1.75, 2)
   fillBackground()
 
   uniformVec2(starfieldScroll, elapsed * 0.1, 0)
   uniformVec2(starfieldScale, 2.1, 2.1)
   uniformFloat(starfieldRadius, 0.1)
-  uniformVec3(starfieldColor, 2, 2, 2)
+  uniformVec3(starfieldColor, 1, 1.5, 2)
   fillBackground()
 
   alphaBlending()
