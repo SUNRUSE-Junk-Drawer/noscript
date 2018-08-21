e874147268c4be796c01b84655157493d8110d84
jameswilddev <jameswilddev@outlook.com>
2018-08-21 11:50:11 +0000

    Fixed an empty array of errors being misinterpreted as an error.

--- build/cssCompressors.js
+++ build/cssCompressors.js
@@ -10,7 +10,7 @@ export default {
       }
     }).minify(css)
 
-    if (minified.errors) {
+    if (minified.errors.length) {
       onError(minified.errors)
     } else {
       onSuccess(minified.styles)
