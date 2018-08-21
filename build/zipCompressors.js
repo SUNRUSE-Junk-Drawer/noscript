cd72e2750670ed23ace4eb5047c5f8e0613031b0
jameswilddev <jameswilddev@outlook.com>
2018-08-21 07:28:26 +0100

    Zip compressors are now consistent with the other compressors.

--- build/zipCompressors.js
+++ build/zipCompressors.js
@@ -1,22 +1,19 @@
 import archiver from "archiver"
 import bl from "bl"
-import BuildStage from "./buildStage"
 
 export default {
-  archiver: class ArchiverBuildStage extends BuildStage {
-    constructor(parent, namePrefix) {
-      super(parent, `${namePrefix}Archiver`, [htmlGenerator], false)
-      this.htmlGenerator = htmlGenerator
+  archiver(buffersByFilename, onSuccess, onError) {
+    const archive = archiver(`zip`)
+    for (const filename in buffersByFilename) {
+      archive.append(buffersByFilename[filename], { name: filename })
     }
-
-    performStart() {
-      const archive = archiver(`zip`)
-      archive.append(Buffer.from(this.htmlGenerator.html, `utf8`), { name: `index.html` })
-      archive.pipe(bl((error, data) => this.handle(error, () => {
-        this.zipped = data
-        this.done()
-      })))
-      archive.finalize()
-    }
+    archive.pipe(bl((error, data) => {
+      if (error) {
+        onError(error)
+      } else {
+        onSuccess(data)
+      }
+    }))
+    archive.finalize()
   }
 }
